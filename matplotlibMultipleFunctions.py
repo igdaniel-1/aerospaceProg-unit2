9f7f971190a442850298dd4790270ba08041c444
India Daniel <72756858+igdaniel-1@users.noreply.github.com>
2025-11-13 16:20:26 -0500

    plotting multiple functions

--- matplotlibMultipleFunctions.py
+++ matplotlibMultipleFunctions.py
@@ -10,3 +10,20 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+x = np.linspace(0, 2*math.pi, 50)
+y1 = np.tan(x)
+y2 = np.cos(x)
+
+# enable grid 
+plt.grid(True)
+
+# Customize appearance
+plt.plot(x, y1, color='red')
+plt.plot(x, y2, color='blue')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('tan(x) and cos(x)')
+
+plt.show()
